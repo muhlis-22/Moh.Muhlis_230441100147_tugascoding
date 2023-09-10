deae5964b48ae33d83d92a3dae99e5f39298c035
muhlis-22 <143861063+muhlis-22@users.noreply.github.com>
2023-09-10 14:54:08 +0700

    Add files via upload

---
+++ Tugas_pelatihan_coding.py
@@ -0,0 +1,35 @@
+# mengkonfersi celcius ke fahrenheit
+
+# suhu dalam Celsius
+celsius = 10
+
+# konversi ke Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# hasil
+
+print("hasil dari konfersi 10 celsius ke fahrenheit adalah", fahrenheit )
+
+
+# mengkonfersi fahrenheit ke celcius
+
+# Suhu dalam Fahrenheit
+fahrenheit = 50
+
+# Konversi ke Celsius
+celsius = (5/9) * (fahrenheit - 32)
+
+# hasil
+print(f"{fahrenheit} Fahrenheit sama dengan {celsius} Celsius")
+
+
+# mengkonfersi kelvin ke celcius
+
+# suhu dalam Kelvin
+suhu_kelvin = 318.15
+
+# Konversi ke Celsius
+suhu_celsius = suhu_kelvin - 273.15
+
+# Cetak hasil konversi
+print("jadi Suhu kelvin dalam Celsius adalah :", suhu_celsius, "°C")
